e538cf36371404a3dc2f6fc7dec5c109ba49e686
Van Chi Hieu <vanchihieu3@gmail.com>
2023-08-13 11:48:24 +0700

    finish

--- src/components/Keyboard/KeyBoard.tsx
+++ src/components/Keyboard/KeyBoard.tsx
@@ -3,11 +3,16 @@ import Key from "../Key/Key";
 import "./keyboard.css";
 import { useSelector, useDispatch } from "react-redux";
 import { rootState } from "../interface";
-import { decPos, setBoard } from "../../redux/boardSlice";
-
+import { decPos, incRow, setBoard } from "../../redux/boardSlice";
+import wordList from "../../words.json";
 const KeyBoard: React.FC = () => {
     const board = useSelector((state: rootState) => state.board.board);
     const position = useSelector((state: rootState) => state.board.pos);
+    const row = useSelector((state: rootState) => state.board.row);
+    const correctWord = useSelector(
+        (state: rootState) => state.board.correctWord
+    );
+
     const dispatch = useDispatch();
 
     const rows: string[] = [
@@ -15,13 +20,32 @@ const KeyBoard: React.FC = () => {
         "a s d f g h j k l",
         "z x c v b n m",
     ];
+    let allWords: string[] = wordList.words;
+    let board5Words: string = `${board[position - 5]}${board[position - 4]}${
+        board[position - 3]
+    }${board[position - 2]}${board[position - 1]}`.toLowerCase();
+
     const clickBack = () => {
-        if (position <= 0) return;
+        if (Math.floor((position - 1) / 5) < row) return;
 
         const newBoard = [...board];
         newBoard[position - 1] = "";
         dispatch(decPos());
         dispatch(setBoard(newBoard));
+    };
+
+    const clickEnter = () => {
+        if (allWords.includes(board5Words) === false) {
+            alert("Invalid words");
+        }
+        if (allWords.includes(board5Words)) {
+            if (position % 5 === 0 && position !== 0) {
+                dispatch(incRow());
+            }
+        }
+        if (position === 30 && allWords.includes(board5Words)) {
+            alert("The word is: " + correctWord);
+        }
     };
     return (
         <div className="keyboard-container">
@@ -29,7 +53,9 @@ const KeyBoard: React.FC = () => {
                 return (
                     <div className="row">
                         {index === 2 && (
-                            <span className="letter-row">Enter</span>
+                            <span className="letter-row" onClick={clickEnter}>
+                                Enter
+                            </span>
                         )}
                         {row.split(" ").map((letter, index) => {
                             return (
--- src/redux/boardSlice.ts
+++ src/redux/boardSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import wordList from '../words.json'
 
+let randomNum = Math.floor(Math.random() * wordList.words.length)
 const initialState = {
     board: [
         "",
@@ -34,6 +36,8 @@ const initialState = {
         "",
     ],
     pos: 0,
+    row: 0,
+    correctWord: wordList.words[randomNum].toUpperCase()
 };
 
 export const boardSlice = createSlice({
@@ -46,11 +50,14 @@ export const boardSlice = createSlice({
         incPos: (state) => {
             state.pos++;
         },
-        decPos: (state) =>{
+        decPos: (state) => {
             state.pos--;
-        }
+        },
+        incRow: (state) => {
+            state.row++;
+        },
     },
 });
 
-export const { setBoard, incPos, decPos } = boardSlice.actions;
+export const { setBoard, incPos, decPos, incRow } = boardSlice.actions;
 export default boardSlice.reducer;
